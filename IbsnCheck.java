d74d7c81750daeff0408d1552259a2deee142474
CRMiteJCodes <crmithun.shanjai@gmail.com>
2025-04-14 17:22:49 +0530

    Update IbsnCheck.java

--- IbsnCheck.java
+++ IbsnCheck.java
@@ -1,3 +1,6 @@
+//ISBN is a ten-digit unique number. With the help of the ISBN, we can easily find any book. 
+//The ISBN number is a legal number when 1*Digit1 + 2*Digit2 + 3*Digit3 + 4*Digit4 + 5*Digit5 + 6*Digit6 + 7*Digit7 + 8*Digit8 + 9*Digit9 + 10*Digit10 is divisible by 11. 
+//The digits are taken from right to left. So, if the ten-digit number is 7426985414, Digit1 and Digit10 will be 4 and 7, respectively.
 import java.util.Scanner;
 public class IbsnCheck
 {
